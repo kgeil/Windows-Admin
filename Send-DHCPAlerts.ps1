bcebdfe857792263fa772cc253cbe3db34d99264
kgeil <kgeil@orda.org>
2019-03-27 11:43:00 -0400

    Added support for log file path with LogFilePath parameter

--- Send-DHCPAlerts.ps1
+++ Send-DHCPAlerts.ps1
@@ -5,11 +5,12 @@
       DHCPEmail -DhcpServer MyDHCPServer -scope 10.50.0.0 -SMTPServer MySMTPServer -FromEmail monitoring@example.org -ToEmail admin1@example.org,admin2@example.org
 
 #>
-#TODO 1. Provide parameter for log file path
-#TODO 2. Make email functionality optional
-#TODO 3. Add send-to-syslog functionality, likely using Jason Fossen's script
-Function DHCPEmail ($DhcpServer, $scope, $SMTPServer, $FromEmail, $ToEmail)
-{
+#TODO 1. Make email functionality optional
+#TODO 2. Add send-to-syslog functionality, likely using Jason Fossen's script
+
+param($DhcpServer,$scope, $SMTPServer,$FromEmail,$ToEmail,$LogFilePath)
+
+function Send-DhcpAlert ($DhcpServer, $scope, $SMTPServer, $FromEmail, $ToEmail, $LogFilePath) {
 $Header = @"
 <style>
 TABLE {border-width: 1px; border-style: solid; border-color: black; border-collapse: collapse;}
@@ -20,18 +21,17 @@ TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
   $datetime= Get-Date #| Out-String
   $LeaseInfo = Get-DhcpServerv4Lease -scopeID $scope -ComputerName $DhcpServer | select -Property ClientID, Hostname, AddressState | where {$_.AddressState -eq "Active"}
   $Body = $LeaseInfo | ConvertTo-Html -Head $Header
-  IF ($LeaseInfo) 
-  {
-      Send-MailMessage  -SmtpServer $SMTPServer -From $FromEmail -To $ToEmail -Subject "DHCP Lease without Reservation Detected" -BodyAsHtml "$Body"
-          ForEach($_ in $leaseinfo)
-              {"$datetime DHCP Lease without Reservation Detected $_" | Out-File -Append -FilePath .\dhcpscript.log}  
-  }
-    Else 
-    {
-    #$outstring = $datetime 
-    "$datetime  No non-reserved dhcp leases detected for $dhcpserver for scope $scope" | Out-File -Append -FilePath .\dhcpscript.log  
-    }
-  #$LeaseInfo = $null
+  IF (!$LogFilePath) {$LogFilePath = (Get-Item -Path ".\").FullName}
+      IF ($LeaseInfo) 
+      {
+          Send-MailMessage  -SmtpServer $SMTPServer -From $FromEmail -To $ToEmail -Subject "DHCP Lease without Reservation Detected" -BodyAsHtml "$Body"
+              ForEach($_ in $leaseinfo)
+                  {"$datetime DHCP Lease without Reservation Detected $_" | Out-File -Append -FilePath $LogFilePath\dhcpscript.log}  
+      }
+      Else 
+      {
+        "$datetime  No non-reserved dhcp leases detected for $dhcpserver for scope $scope" | Out-File -Append -FilePath $LogFilePath\dhcpscript.log  
+      }
 }
 
-
+Send-DhcpAlert -DhcpServer $DhcpServer -scope $scope -SMTPServer $SMTPServer -FromEmail $FromEmail -ToEmail $ToEmail -LogFilePath $LogFilePath
